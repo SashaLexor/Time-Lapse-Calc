baab61908de2437de1d956bd8aa61c4bdedeace1
Alex <sashalexor@gmail.com>
2016-07-21 16:53:18 +0300

    Initial Commit

---
+++ Time Lapse Calc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Time Lapse Calc
+//
+//  Created by 1024 on 21.07.16.
+//  Copyright © 2016 Sasha Lexor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
